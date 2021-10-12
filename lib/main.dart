fd551439b895a689cf941761720a229ef8d41558
VoThanhPhi666 <vothanhphi7189@gmail.com>
2021-10-12 23:40:18 +0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your applicationn.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
